b199f1679ba402dfa87647685769590d7a7bec12
Tahlia <tbolden13@gmail.com>
2022-11-07 15:03:29 -0800

    working on functions in serverjs

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 const cTable = require('console.table');
-const { start } = require('repl');
+//const { start } = require('repl');
 
 //connection for sql database
 const connection = mysql.createConnection({
@@ -21,3 +21,69 @@ connection.connect(function(err) {
 
     start();
 });
+
+//using CASE expression in sql to go through conditions to see if they are met and a switch case depending on answers
+function start() {
+    inquirer
+    .prompt ([
+        {
+            type: 'list',
+            name: 'start',
+            message: "Welcome to Star Systems database with information on departments, employees and employee roles. What would you like to choose?",
+            choices: ['View', 'Update', 'Add', 'Exit']
+        }
+    ]).then (function(res) {
+        switch(res.start){
+            case 'View':
+                view();
+                break;
+            case 'Update':
+                updateEmployee();
+                break;
+            case 'Add':
+                add();
+                break;
+            case 'Exit':
+                console.log("Live long and have a productive day");
+                console.log('----------------------------------------');
+                break;
+                default:
+                    console.log('default');
+
+            
+        }
+    });
+}
+
+//view function
+function view() {
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'view',
+            message: 'Please choose one to view',
+            choices: ['Employee Roster', 'By Role', 'By Department']
+        }
+    ]).then(function(res){
+        switch(res.view){
+            case 'Employee Roster':
+                viewEmployeeRoster();
+                break;
+            case 'By role':
+                viewByRole();
+            case 'By Department':
+                viewByDepartment();
+                default:
+                    console.log('default');
+        }
+    });
+}
+
+//view function breakdown
+function view() {
+
+}
+function viewEmployeeRoster() {
+    connection.query('')
+}
